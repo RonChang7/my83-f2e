d72be08461b98f30d099b0b6b177804d7b8915a1
Charlie Lee <charlie800228@gmail.com>
2020-01-09 11:34:09 +0800

    Add base icon

---
+++ src/components/base/icon/24/BaseClose.vue
@@ -0,0 +1,30 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <g fill="#6C6C6C" fill-rule="evenodd">
+      <rect
+        width="2"
+        height="24"
+        x="11.192"
+        y=".192"
+        rx="1"
+        transform="rotate(45 12.192 12.192)"
+      />
+      <rect
+        width="2"
+        height="24"
+        x="11.192"
+        y=".192"
+        rx="1"
+        transform="rotate(135 12.192 12.192)"
+      />
+    </g>
+  </svg>
+</template>
+<script>
+export default {}
+</script>
---
+++ src/components/base/icon/24/BaseMenu.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <g
+      fill="none"
+      fill-rule="evenodd"
+      stroke="#6C6C6C"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+    >
+      <path d="M3 5.5h17.5M3 12.5h17.5M3 19.5h17.5" />
+    </g>
+  </svg>
+</template>
+<script>
+export default {}
+</script>
