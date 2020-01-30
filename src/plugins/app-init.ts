59704fd5ef2b00ea10b71b99c1f3fa12f8e5ff33
Charlie Lee <charlie800228@gmail.com>
2020-01-30 12:50:21 +0800

    Modify app-init with global store and header api action

--- src/plugins/app-init.ts
+++ src/plugins/app-init.ts
@@ -1,23 +1,44 @@
+import _ from 'lodash'
 import { Plugin as NuxtPlugin } from '@nuxt/types'
+import { createStoreModule as createGlobalStoreModule } from '@/store/global/index'
 import { createStoreModule as createHeaderStoreModule } from '@/store/header/index'
 import * as types from '@/store/header/header.type'
-import { User } from '@/modules/user/user'
+import { User } from '@/services/user/user'
 
 // import global types
 import '@/types/ua.type'
 
-export default (({ app, store }) => {
-  const moduleName = 'header'
-  User.getInstance().UpdateUser()
+const storeModules = {
+  global: {
+    moduleName: 'global',
+    createModule: () => createGlobalStoreModule(),
+  },
+  header: {
+    moduleName: 'header',
+    createModule: () => createHeaderStoreModule(),
+  },
+}
 
-  app.$registerStore({
-    module: createHeaderStoreModule(),
-    moduleName,
-    store: app.store,
+export default (async ({ app, store }) => {
+  const user = User.getInstance()
+  user.updateUser()
+
+  _.forEach(storeModules, ({ createModule, moduleName }) => {
+    app.$registerStore({
+      module: createModule(),
+      moduleName,
+      store: app.store,
+    })
   })
 
-  // Server 預先打一次 header，到 client 端後，如果身份是 sales ，另外在打一次 API
-  if (process.server || User.role === 'sales') {
-    store.dispatch(`${moduleName}/${types.FETCH_HEADER_NAV_DATA}`)
+  // Server pre-fetch Header data for SEO performance
+  if (process.server) {
+    await store.dispatch(
+      `${storeModules.header.moduleName}/${types.FETCH_HEADER_NAV_DATA}`
+    )
+  }
+
+  if (process.client && User.role === 'guest') {
+    user.updateLandingUrl()
   }
 }) as NuxtPlugin
