9ab4e2c1a6ae7b5f339a30d414f890b6b12573ed
Charlie Lee <charlie800228@gmail.com>
2020-02-06 17:27:25 +0800

    feat: add question section
    author, title, content

---
+++ app/src/components/question/question/QuestionAuthorInfo.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="QuestionAuthorInfo">
+    <img :src="avatar" alt="avatar" class="avatar" />
+    {{ name }}·
+    <GlobalLink :to="target.link.path">{{ target.name }}</GlobalLink>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { ThisTypedComponentOptionsWithRecordProps } from 'vue/types/options'
+import { CombinedVueInstance } from 'vue/types/vue'
+import { Tag } from '@/api/question/question.type'
+import GlobalLink from '@/components/base/global-link/GlobalLink.vue'
+
+export default {
+  components: {
+    GlobalLink,
+  },
+  props: {
+    avatar: {
+      type: String,
+      default: '',
+    },
+    name: {
+      type: String,
+      default: '',
+    },
+    target: {
+      type: Object,
+      default: () => {},
+    },
+  },
+} as ComponentOption
+
+export type ComponentOption = ThisTypedComponentOptionsWithRecordProps<
+  Instance,
+  Data,
+  Methods,
+  Computed,
+  Props
+>
+
+export type ComponentInstance = CombinedVueInstance<
+  Instance,
+  Data,
+  Methods,
+  Computed,
+  Props
+>
+
+export interface Instance extends Vue {}
+
+export interface Data {}
+
+export interface Methods {}
+
+export interface Computed {}
+
+export interface Props {
+  avatar: string
+  name: string
+  target: Tag
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/sass/variables.scss';
+
+.QuestionAuthorInfo {
+  display: flex;
+  align-items: center;
+  font-size: 0.875rem;
+
+  .avatar {
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    margin-right: 8px;
+  }
+
+  &::v-deep a {
+    color: $gray-secondary;
+
+    &:hover {
+      color: $primary-hover-color;
+    }
+  }
+}
+</style>
---
+++ app/src/components/question/question/QuestionContent.vue
@@ -0,0 +1,33 @@
+<template functional>
+  <div v-html="props.content" class="QuestionContent" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { FunctionalComponentOptions } from 'vue/types/options'
+import { CombinedVueInstance } from 'vue/types/vue'
+
+export default {
+  functional: true,
+} as ComponentOption
+
+export type ComponentOption = FunctionalComponentOptions<Props>
+
+export type ComponentInstance = CombinedVueInstance<Instance, {}, {}, {}, Props>
+
+export interface Instance extends Vue {}
+
+export interface Props {
+  content: string
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/sass/variables.scss';
+@import '@/sass/rwd.scss';
+
+.QuestionContent {
+  color: $gray-primary;
+  margin-bottom: 24px;
+}
+</style>
