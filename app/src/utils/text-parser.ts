2229ab3ca43a778580cb477403bf7e03a7418600
Charlie Lee <charlie800228@gmail.com>
2020-02-06 17:26:37 +0800

    feat: add package: linkifyjs

---
+++ app/src/utils/text-parser.ts
@@ -0,0 +1,15 @@
+import linkifyStr from 'linkifyjs/string'
+
+export const textToUrl = (content: string) => {
+  const string = linkifyStr(content, {
+    className: '',
+    nl2br: true,
+    validate: {
+      url: (value) => /^https?:\/\//.test(value),
+      email: () => false,
+    },
+  })
+  return string.replace(/&amp;/g, '&')
+}
+
+export const nl2br = (content: string) => content.replace(/\n|\r\n?/g, '<br />')
