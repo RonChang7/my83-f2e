e076f17042b3a262d029f5472c8e32a00a214628
Charlie Lee <charlie800228@gmail.com>
2020-02-19 11:15:52 +0800

    feat: question init page init setting

--- app/src/views/question/page/Index.vue
+++ app/src/views/question/page/Index.vue
@@ -5,7 +5,12 @@ import { CombinedVueInstance } from 'vue/types/vue'
 import {
   FETCH_QUESTION_DATA,
   FETCH_ANSWER_DATA,
+  HIGHLIGHT_BEST_ANSWER,
+  FETCH_QUESTION_PERSONALIZE_DATA,
+  FETCH_ANSWER_PERSONALIZE_DATA,
 } from '@/store/question/question.type'
+import { State } from '@/store/question/index'
+import { User } from '@/services/user/user'
 const QuestionPage = () => import('./QuestionPage.vue')
 
 export default {
@@ -15,6 +20,22 @@ export default {
       store.dispatch(`question/${FETCH_QUESTION_DATA}`, id),
       store.dispatch(`question/${FETCH_ANSWER_DATA}`, id),
     ])
+
+    const { question } = store.state.question as State
+
+    if (question && question.best_answer_id) {
+      store.dispatch(
+        `question/${HIGHLIGHT_BEST_ANSWER}`,
+        question.best_answer_id
+      )
+    }
+  },
+  mounted() {
+    if (User.role !== 'guest') {
+      const id = this.$route.params.id
+      this.$store.dispatch(`question/${FETCH_QUESTION_PERSONALIZE_DATA}`, id)
+      this.$store.dispatch(`question/${FETCH_ANSWER_PERSONALIZE_DATA}`, id)
+    }
   },
   render(h) {
     return h(QuestionPage)
