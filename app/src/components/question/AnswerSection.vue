f1fc2f4b52a4d6c4ea692259a10b0420ef8fc32b
Charlie Lee <charlie800228@gmail.com>
2020-02-19 11:11:58 +0800

    feat: add user function
    question personalize
    answer personalize
    follow/unfollow question
    set/unset best answer
    
    feat: add base more button

--- app/src/components/question/AnswerSection.vue
+++ app/src/components/question/AnswerSection.vue
@@ -3,27 +3,39 @@
     <div v-if="shouldShowAnswerCount" class="AnswerSection__title">
       共 {{ answerCount }} 則留言
     </div>
-    <div class="AnswerSection__header">
-      <BaseAuthorInfo :author-info="answer.author_info" />
+    <div class="AnswerSection__content" :class="{ bestAnswer: isBestAnswer }">
+      <div v-if="isBestAnswer" class="AnswerSection__bestAnswer">最佳留言</div>
+      <div class="AnswerSection__header">
+        <BaseAuthorInfo :author-info="answer.author_info" />
+        <BaseHeaderFunction
+          section-type="answer"
+          :section-id="answer.answer_id"
+          :author-info="answer.author_info"
+          :personalize="answer.personalize"
+          :is-best-answer="isBestAnswer"
+          :is-question-author="isQuestionAuthor"
+          :question-id="questionId"
+        />
+      </div>
+      <BaseContent :content="answer.content" />
+      <BaseMeta
+        :like-count="answer.answer_meta.like_count"
+        :dislike-count="answer.answer_meta.dislike_count"
+        :answer-count="answer.answer_meta.response_count"
+        :created-at="answer.created_at"
+        meta-type="answer"
+      />
+      <AnswerInteraction @action="buttonActionHandler" />
+      <ResponsesSection :responses="answer.responses" />
+      <ResponseEditor
+        v-if="displayResponsePanel"
+        :avatar="avatar"
+        :nickname="nickname"
+        :has-response="!!answer.responses.length"
+        :active-panel.sync="activeResponsePanel"
+        :is-focus.sync="isResponsePanelFocus"
+      />
     </div>
-    <BaseContent :content="answer.content" />
-    <BaseMeta
-      :like-count="answer.answer_meta.like_count"
-      :dislike-count="answer.answer_meta.dislike_count"
-      :answer-count="answer.answer_meta.response_count"
-      :created-at="answer.created_at"
-      meta-type="answer"
-    />
-    <AnswerInteraction @action="buttonActionHandler" />
-    <ResponsesSection :responses="answer.responses" />
-    <ResponseEditor
-      v-if="displayResponsePanel"
-      :avatar="avatar"
-      :nickname="nickname"
-      :has-response="!!answer.responses.length"
-      :active-panel.sync="activeResponsePanel"
-      :is-focus.sync="isResponsePanelFocus"
-    />
   </div>
 </template>
 
@@ -37,6 +49,8 @@ import { Type } from './answer/AnswerInteractionButton.vue'
 import ResponsesSection from './ResponsesSection.vue'
 import BaseContent from './base/BaseContent.vue'
 import BaseMeta from './base/BaseMeta.vue'
+import BaseHeaderFunction from './base/BaseHeaderFunction.vue'
+import { AvatarMap } from './helpers/reply-default-avatar'
 import BaseCard from '@/components/my83-ui-kit/card/BaseCard.vue'
 import { AnswerData } from '@/api/question/question.type'
 import { User } from '@/services/user/user'
@@ -44,12 +58,6 @@ import { State } from '@/store/header/index'
 const ResponseEditor = () => import('./response/ResponseEditor.vue')
 
 const UserRole = User.role
-const AvatarMap = {
-  sales: '/images/avatar/sales_girl4.png',
-  client: '/images/avatar/client_girl1.png',
-  admin: '/images/avatar/client_girl1.png',
-  guest: '',
-}
 
 export default {
   components: {
@@ -60,6 +68,7 @@ export default {
     AnswerInteraction,
     ResponsesSection,
     ResponseEditor,
+    BaseHeaderFunction,
   },
   props: {
     answer: {
@@ -74,13 +83,25 @@ export default {
       type: Number,
       default: 0,
     },
+    isBestAnswer: {
+      type: Boolean,
+      default: false,
+    },
+    isQuestionAuthor: {
+      type: Boolean,
+      default: false,
+    },
+    questionId: {
+      type: Number,
+      default: 0,
+    },
   },
   data() {
     return {
       openEditor: false,
-      avatar: AvatarMap[UserRole],
       activeResponsePanel: false,
       isResponsePanelFocus: false,
+      avatar: AvatarMap[UserRole],
     }
   },
   computed: {
@@ -133,9 +154,9 @@ export interface Instance extends Vue {}
 
 export interface Data {
   openEditor: boolean
-  avatar: string
   activeResponsePanel: boolean
   isResponsePanelFocus: boolean
+  avatar: string
 }
 
 export interface Methods {
@@ -153,6 +174,9 @@ export interface Props {
   answer: AnswerData
   shouldShowAnswerCount: boolean
   answerCount: number
+  isBestAnswer: boolean
+  isQuestionAuthor: boolean
+  questionId: number
 }
 </script>
 
@@ -167,7 +191,6 @@ export interface Props {
   flex: 1 0 auto;
   display: flex;
   flex-direction: column;
-  padding: 0 30px;
   margin-bottom: 20px;
 
   &__title {
@@ -176,12 +199,35 @@ export interface Props {
     color: $gray-primary;
     height: 50px;
     border-bottom: 1px solid $gray-quaternary;
-    margin: 0 -30px;
     padding: 0 30px;
   }
 
   &__header {
+    display: flex;
+    justify-content: space-between;
     margin: 30px 0 15px;
+  }
+
+  &__bestAnswer {
+    display: flex;
+    align-items: center;
+    font-weight: 500;
+    height: 50px;
+    background: #ffeadf;
+    color: $primary-color;
+    margin: 0 -28px;
+    padding: 0 28px;
+    border-radius: 2px 2px 0 0;
+  }
+
+  &__content {
+    padding: 0 30px;
+
+    &.bestAnswer {
+      padding: 0 28px;
+      border: 2px solid $primary-color;
+      border-radius: $border-radius;
+    }
   }
 }
 </style>
---
+++ app/src/components/question/helpers/reply-default-avatar.ts
@@ -0,0 +1,8 @@
+import { Role } from '@/services/user/user'
+
+export const AvatarMap: Record<Role, string> = {
+  guest: '',
+  client: '/images/avatar/client_girl1.png',
+  sales: '/images/avatar/sales_girl4.png',
+  admin: '/images/avatar/client_girl1.png',
+}
