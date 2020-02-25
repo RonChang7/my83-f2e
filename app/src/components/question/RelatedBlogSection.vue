ddf73317f029fac547c706d0c05198dea77bbbc2
Charlie Lee <charlie800228@gmail.com>
2020-02-25 11:34:00 +0800

    feat: add related blog & question

---
+++ app/src/components/question/RelatedBlogSection.vue
@@ -0,0 +1,65 @@
+<template>
+  <RelatedSection
+    v-if="relatedBlogs.length"
+    :related-data="relatedBlogs"
+    :max-post="maxPost"
+    title="相關文章"
+  />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { ThisTypedComponentOptionsWithRecordProps } from 'vue/types/options'
+import { CombinedVueInstance } from 'vue/types/vue'
+import RelatedSection from './RelatedSection.vue'
+import { State } from '@/store/question/index'
+import { RelatedBlog } from '@/api/question/question.type'
+
+export default {
+  components: {
+    RelatedSection,
+  },
+  props: {
+    maxPost: {
+      type: Number,
+      default: 10,
+    },
+  },
+  computed: {
+    relatedBlogs() {
+      const { relatedBlogs } = this.$store.state.question as State
+      return relatedBlogs || []
+    },
+  },
+} as ComponentOption
+
+export type ComponentOption = ThisTypedComponentOptionsWithRecordProps<
+  Instance,
+  Data,
+  Methods,
+  Computed,
+  Props
+>
+
+export type ComponentInstance = CombinedVueInstance<
+  Instance,
+  Data,
+  Methods,
+  Computed,
+  Props
+>
+
+export interface Instance extends Vue {}
+
+export interface Data {}
+
+export interface Methods {}
+
+export interface Computed {
+  relatedBlogs: RelatedBlog[]
+}
+
+export interface Props {
+  maxPost: number
+}
+</script>
---
+++ app/src/components/question/related/RelatedCard.vue
@@ -0,0 +1,166 @@
+<template>
+  <div class="RelatedCard" :class="{ last }">
+    <div class="RelatedCard__index">{{ index }}</div>
+    <div class="RelatedCard__content">
+      <div class="RelatedCard__title">{{ title }}</div>
+      <div class="RelatedCard__meta">
+        <span class="RelatedCard__count">
+          <BaseView />
+          {{ numberTransform(viewCount) }}
+        </span>
+        <span v-if="answerCount !== null" class="RelatedCard__count">
+          <BaseComment />
+          {{ numberTransform(answerCount) }}
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { ThisTypedComponentOptionsWithRecordProps } from 'vue/types/options'
+import { CombinedVueInstance } from 'vue/types/vue'
+import BaseView from '@/components/base/icon/18/BaseView.vue'
+import BaseComment from '@/components/base/icon/18/BaseComment.vue'
+
+export default {
+  components: {
+    BaseView,
+    BaseComment,
+  },
+  props: {
+    index: {
+      type: Number,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    viewCount: {
+      type: Number,
+      required: true,
+    },
+    answerCount: {
+      type: Number,
+      default: null,
+    },
+    last: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    numberTransform(number) {
+      return number.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+    },
+  },
+} as ComponentOption
+
+export type ComponentOption = ThisTypedComponentOptionsWithRecordProps<
+  Instance,
+  Data,
+  Methods,
+  Computed,
+  Props
+>
+
+export type ComponentInstance = CombinedVueInstance<
+  Instance,
+  Data,
+  Methods,
+  Computed,
+  Props
+>
+
+export interface Instance extends Vue {}
+
+export interface Data {}
+
+export interface Methods {
+  numberTransform(number: number): string
+}
+
+export interface Computed {}
+
+export interface Props {
+  index: number
+  title: string
+  viewCount: number
+  answerCount: number
+  last: boolean
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/sass/variables.scss';
+@import '@/sass/rwd.scss';
+
+.RelatedCard {
+  display: flex;
+  padding: 16px 0 20px;
+  line-height: 1.5;
+  border-bottom: 1px solid $gray-quaternary;
+
+  &.last {
+    border-bottom: 0;
+  }
+
+  @include max-media('xl') {
+    padding: 10px;
+  }
+
+  &__index {
+    color: $primary-color;
+    font-size: 1.25em;
+    font-weight: 500;
+    padding-right: 22px;
+    max-width: 33px;
+
+    @include max-media('xl') {
+      font-size: 1em;
+      max-width: 25px;
+      padding-right: 15px;
+    }
+  }
+
+  &__content {
+    display: flex;
+    flex-direction: column;
+  }
+
+  &__title {
+    color: $gray-primary;
+    margin: 2px 0 6px;
+
+    @include max-media('xl') {
+      margin-top: 0;
+    }
+  }
+
+  &__meta {
+    display: flex;
+    color: $text-default-color;
+  }
+
+  &__count {
+    display: flex;
+    align-items: center;
+    font-size: 0.875rem;
+    margin-right: 20px;
+
+    @include max-media('xl') {
+      margin-right: 30px;
+    }
+
+    &::v-deep svg {
+      margin-right: 6px;
+
+      > path {
+        fill: $gray-tertiary;
+      }
+    }
+  }
+}
+</style>
