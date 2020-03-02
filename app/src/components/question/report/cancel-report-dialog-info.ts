ab252f24fb776b6769f123f2761b0630e32fd5b7
Charlie Lee <charlie800228@gmail.com>
2020-03-02 13:24:13 +0800

    feat: report panel style setting

--- app/src/components/question/report/cancel-report-dialog-info.ts
+++ app/src/components/question/report/cancel-report-dialog-info.ts
@@ -2,6 +2,7 @@ import { GlobalDialogContent } from '@/store/global'
 
 export const CancelReportDialogContent: GlobalDialogContent = {
   title: '取消檢舉',
+  content: '確定要取消檢舉嗎？',
   leftButtonText: '取消',
   leftButtonType: 'secondary',
   rightButtonText: '送出',
