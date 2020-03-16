ab63da4f3ee6dfbb71546689c298cc6de2ae2f58
Charlie Lee <charlie800228@gmail.com>
2020-03-16 16:22:38 +0800

    fix: change logger style

--- app/src/plugins/axios-setup.ts
+++ app/src/plugins/axios-setup.ts
@@ -13,11 +13,8 @@ export default (({ app }) => {
 
   if (APP_ENV === 'development' && !request.initApiUrlLogger) {
     request.interceptors.request.use((v) => {
-      const tagStyle =
-        'background: #7bd6ff; border-radius: 1em; color: #fff; padding: 2px 6px; font-weight: bold;'
-      const contentStyle = 'padding: 2px 6px;'
       // eslint-disable-next-line no-console
-      console.log(`%cAPI Request:%c${v.url}`, tagStyle, contentStyle)
+      console.log(`\x1B[34mAPI Request:\x1B[0m`, v.url)
 
       return v
     })
