99e552adcaababc71a51f32815ca3638437ce48a
Charlie Lee <charlie800228@gmail.com>
2020-03-17 15:27:38 +0800

    fix: adjust default dialog action

--- app/src/components/question/report/cancel-report-dialog-info.ts
+++ app/src/components/question/report/cancel-report-dialog-info.ts
@@ -4,8 +4,5 @@ export const CancelReportDialogContent: GlobalDialogContent = {
   title: '取消檢舉',
   content: '確定要取消檢舉嗎？',
   leftButtonText: '取消',
-  leftButtonType: 'secondary',
   rightButtonText: '送出',
-  rightButtonType: 'primary',
-  defaultActionButton: 'right',
 }
