b59d58eb8710b65073e24a65904c9777d3874b92
Charlie Lee <charlie800228@gmail.com>
2020-03-31 18:21:43 +0800

    fix: handle axios error when no response

--- app/src/plugins/axios-setup.ts
+++ app/src/plugins/axios-setup.ts
@@ -34,41 +34,59 @@ export default (({ app }) => {
       return config
     })
 
+    /**
+     * Axios error handler
+     * https://gist.github.com/fgilio/230ccd514e9381fafa51608fcf137253
+     */
     request.interceptors.response.use(
       (res) => res,
       (err) => {
-        const {
-          config,
-          response: {
-            status,
-            data: { error },
-          },
-        } = err
+        if (err.response) {
+          const {
+            config,
+            response: {
+              status,
+              data: { error },
+            },
+          } = err
 
-        const originalRequest = config as AxiosRequestConfig
-        const auth = Auth.getInstance()
+          const originalRequest = config as AxiosRequestConfig
+          const auth = Auth.getInstance()
 
-        if (
-          preventInterceptorsList.find(
-            (url) => API_URL + url === originalRequest.url
-          )
-        ) {
-          return Promise.reject(err)
-        } else if (status === 401 && error === 'expired_token') {
-          return JWT.refreshToken(API_URL)
-            .then(() => {
-              const jwtToken = auth.getToken()
-              originalRequest.headers.Authorization = `Bearer ${jwtToken}`
-              return request(originalRequest)
-            })
-            .catch((err) => {
-              return Promise.reject(err)
-            })
-        } else if (status === 401 && error === 'invalid_token') {
-          Suspect.setRoleCode()
-          auth.logout()
-          return Promise.reject(err)
+          if (
+            preventInterceptorsList.find(
+              (url) => API_URL + url === originalRequest.url
+            )
+          ) {
+            return Promise.reject(err)
+          } else if (status === 401 && error === 'expired_token') {
+            return JWT.refreshToken(API_URL)
+              .then(() => {
+                const jwtToken = auth.getToken()
+                originalRequest.headers.Authorization = `Bearer ${jwtToken}`
+                return request(originalRequest)
+              })
+              .catch((err) => {
+                return Promise.reject(err)
+              })
+          } else if (status === 401 && error === 'invalid_token') {
+            Suspect.setRoleCode()
+            auth.logout()
+            return Promise.reject(err)
+          } else {
+            return Promise.reject(err)
+          }
         } else {
+          err = {
+            ...err,
+            response: {
+              status: 0,
+              data: {
+                success: false,
+                message: '發生問題，請稍後再試',
+              },
+            },
+          }
           return Promise.reject(err)
         }
       }
