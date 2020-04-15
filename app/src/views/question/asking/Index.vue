9e4844bc07ca43ee7888fd36d57a4e3cf0ab4835
Charlie Lee <charlie800228@gmail.com>
2020-04-15 18:13:04 +0800

    feat(asking): add question asking page

---
+++ app/src/views/question/asking/Index.vue
@@ -0,0 +1,46 @@
+<script lang="ts">
+import Vue from 'vue'
+import { ThisTypedComponentOptionsWithRecordProps } from 'vue/types/options'
+import { CombinedVueInstance } from 'vue/types/vue'
+import { UPDATE_PAGE_META } from '@/store/seo/seo.type'
+const AskingPage = () => import('./AskingPage.vue')
+
+const options: ComponentOption = {
+  created() {
+    this.$store.dispatch(`pageMeta/${UPDATE_PAGE_META}`, {
+      title: '發文到 MY83 討論區 - 《MY83 保險討論區》',
+    })
+  },
+  render(h) {
+    return h(AskingPage)
+  },
+}
+
+export type ComponentOption = ThisTypedComponentOptionsWithRecordProps<
+  Instance,
+  Data,
+  Methods,
+  Computed,
+  Props
+>
+
+export type ComponentInstance = CombinedVueInstance<
+  Instance,
+  Data,
+  Methods,
+  Computed,
+  Props
+>
+
+export interface Instance extends Vue {}
+
+export interface Data {}
+
+export interface Methods {}
+
+export interface Computed {}
+
+export interface Props {}
+
+export default options
+</script>
