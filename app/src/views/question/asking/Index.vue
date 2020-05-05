0539e67d2b4829af992ee9527e6090e88d615aef
Charlie Lee <charlie800228@gmail.com>
2020-05-05 23:30:04 +0800

    feat: add form option api

--- app/src/views/question/asking/Index.vue
+++ app/src/views/question/asking/Index.vue
@@ -3,16 +3,31 @@ import Vue from 'vue'
 import { ThisTypedComponentOptionsWithRecordProps } from 'vue/types/options'
 import { CombinedVueInstance } from 'vue/types/vue'
 import { UPDATE_PAGE_META } from '@/store/seo/seo.type'
+import { fetchAskingFormOption } from '@/api/question/asking'
+import { AskingFormOptionResponse } from '@/api/question/asking.type'
 const AskingPage = () => import('./AskingPage.vue')
 
 const options: ComponentOption = {
+  async fetch() {
+    this.formOption = await fetchAskingFormOption()
+  },
+  fetchOnServer: false,
+  data() {
+    return {
+      formOption: {},
+    }
+  },
   created() {
     this.$store.dispatch(`pageMeta/${UPDATE_PAGE_META}`, {
       title: '發文到 MY83 討論區 - 《MY83 保險討論區》',
     })
   },
   render(h) {
-    return h(AskingPage)
+    return h(AskingPage, {
+      props: {
+        formOption: this.formOption,
+      },
+    })
   },
 }
 
@@ -34,7 +49,9 @@ export type ComponentInstance = CombinedVueInstance<
 
 export interface Instance extends Vue {}
 
-export interface Data {}
+export interface Data {
+  formOption: AskingFormOptionResponse | {}
+}
 
 export interface Methods {}
 
