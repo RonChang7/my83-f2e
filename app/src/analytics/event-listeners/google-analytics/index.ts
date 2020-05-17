f0ae18fe4a493a2378b9145c1930db0cf11b1ec6
Charlie Lee <charlie800228@gmail.com>
2020-05-17 23:00:45 +0800

    doc: add tracking comment

--- app/src/analytics/event-listeners/google-analytics/index.ts
+++ app/src/analytics/event-listeners/google-analytics/index.ts
@@ -1,7 +1,12 @@
 import { GoogleAnalytics } from '../../implementations/google-analytics'
 import { EventTypes, Handlers } from '../event.type'
 
-// MY83-rt 舊有邏輯： role 為數字 or '訪客'
+/**
+ * MY83-rt 舊有邏輯： role 為數字 or '訪客'
+ * role: '訪客' 為 '訪客'
+ * role: 0 為 '保戶' or '管理員' (之前有 2 的身份表示管理員，到新版之後整合為 0)
+ * role: 1 為 '業務員'
+ */
 export const setRole: Handlers[EventTypes.SetRole] = ({ role }) => {
   const ga = GoogleAnalytics.getInstance()
   ga.set({
