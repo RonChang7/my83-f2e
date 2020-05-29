47773e27d5b4c42dbd6644ff0f7f83c3ab3546cb
Charlie Lee <charlie800228@gmail.com>
2020-05-29 11:44:46 +0800

    feat: add question list entry

---
+++ app/src/views/question/list/Index.vue
@@ -0,0 +1,45 @@
+<script lang="ts">
+import Vue from 'vue'
+import { ThisTypedComponentOptionsWithRecordProps } from 'vue/types/options'
+import { CombinedVueInstance } from 'vue/types/vue'
+import { GlobalVuexState } from '@/store/global-state'
+import { State } from '@/store/question/index'
+const ListPage = () => import('./QuestionListPage.vue')
+
+export default {
+  mounted() {},
+  render(h) {
+    return h(ListPage)
+  },
+} as ComponentOption
+
+export type ComponentOption = ThisTypedComponentOptionsWithRecordProps<
+  Instance,
+  Data,
+  Methods,
+  Computed,
+  Props
+>
+
+export type ComponentInstance = CombinedVueInstance<
+  Instance,
+  Data,
+  Methods,
+  Computed,
+  Props
+>
+
+export interface Instance extends Vue {}
+
+export interface Data {}
+
+export interface Methods {}
+
+export interface Computed {}
+
+export interface Props {}
+
+export interface QuestionListVuexState extends GlobalVuexState {
+  question: State
+}
+</script>
