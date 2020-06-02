9461054a904a9e1bf4dc6a4569f9d36d964ed073
Charlie Lee <charlie800228@gmail.com>
2020-06-02 18:39:17 +0800

    feat: add popular qa/blog section

---
+++ app/src/components/question/section/PopularBlogSection.vue
@@ -0,0 +1,67 @@
+<template>
+  <RelatedSection
+    v-if="popularBlogs.length"
+    :related-data="popularBlogs"
+    :max-post="maxPost"
+    title="熱門文章"
+  />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Store } from 'vuex'
+import { ThisTypedComponentOptionsWithRecordProps } from 'vue/types/options'
+import { CombinedVueInstance } from 'vue/types/vue'
+import RelatedSection from '../related/RelatedSection.vue'
+import { QuestionListVuexState } from '@/views/question/list/Index.vue'
+import { PopularBlog } from '@/api/question/list.type'
+
+export default {
+  components: {
+    RelatedSection,
+  },
+  props: {
+    maxPost: {
+      type: Number,
+      default: 10,
+    },
+  },
+  computed: {
+    popularBlogs() {
+      return this.$store.state.questionList.popularBlogs || []
+    },
+  },
+} as ComponentOption
+
+export type ComponentOption = ThisTypedComponentOptionsWithRecordProps<
+  Instance,
+  Data,
+  Methods,
+  Computed,
+  Props
+>
+
+export type ComponentInstance = CombinedVueInstance<
+  Instance,
+  Data,
+  Methods,
+  Computed,
+  Props
+>
+
+export interface Instance extends Vue {
+  $store: Store<QuestionListVuexState>
+}
+
+export interface Data {}
+
+export interface Methods {}
+
+export interface Computed {
+  popularBlogs: PopularBlog[]
+}
+
+export interface Props {
+  maxPost: number
+}
+</script>
---
+++ app/src/components/question/section/PopularQuestionSection.vue
@@ -0,0 +1,67 @@
+<template>
+  <RelatedSection
+    v-if="popularQuestions.length"
+    :related-data="popularQuestions"
+    :max-post="maxPost"
+    title="人氣問答"
+  />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Store } from 'vuex'
+import { ThisTypedComponentOptionsWithRecordProps } from 'vue/types/options'
+import { CombinedVueInstance } from 'vue/types/vue'
+import RelatedSection from '../related/RelatedSection.vue'
+import { QuestionListVuexState } from '@/views/question/list/Index.vue'
+import { PopularQuestion } from '@/api/question/list.type'
+
+export default {
+  components: {
+    RelatedSection,
+  },
+  props: {
+    maxPost: {
+      type: Number,
+      default: 10,
+    },
+  },
+  computed: {
+    popularQuestions() {
+      return this.$store.state.questionList.popularQuestions || []
+    },
+  },
+} as ComponentOption
+
+export type ComponentOption = ThisTypedComponentOptionsWithRecordProps<
+  Instance,
+  Data,
+  Methods,
+  Computed,
+  Props
+>
+
+export type ComponentInstance = CombinedVueInstance<
+  Instance,
+  Data,
+  Methods,
+  Computed,
+  Props
+>
+
+export interface Instance extends Vue {
+  $store: Store<QuestionListVuexState>
+}
+
+export interface Data {}
+
+export interface Methods {}
+
+export interface Computed {
+  popularQuestions: PopularQuestion[]
+}
+
+export interface Props {
+  maxPost: number
+}
+</script>
