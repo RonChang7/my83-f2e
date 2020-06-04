676d22198e686d455e73de23da98c3937c961e04
Charlie Lee <charlie800228@gmail.com>
2020-06-04 15:52:08 +0800

    feat: add list store

---
+++ app/src/utils/api-data-transform.ts
@@ -0,0 +1,13 @@
+import { Pagination, PaginationResponse } from '@/api/type'
+
+export const paginationResponseDataTransform = ({
+  current_page,
+  total_page,
+  total_count,
+}: PaginationResponse): Pagination => {
+  return {
+    currentPage: current_page,
+    totalCount: total_page,
+    totalPage: total_count,
+  }
+}
