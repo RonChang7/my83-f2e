439cd31ad1717ea04ce77b8fc96722475474398b
Charlie Lee <charlie800228@gmail.com>
2020-06-04 15:51:33 +0800

    feat: add list API

--- app/src/api/question/list.type.ts
+++ app/src/api/question/list.type.ts
@@ -1,4 +1,10 @@
-import { RelatedBlog, RelatedQuestion } from './question.type'
+import { PaginationResponse } from '../type'
+import { RelatedBlog, RelatedQuestion, QuestionData } from './question.type'
+
+export interface QuestionListResponse {
+  data: QuestionListData[]
+  meta: QuestionListMeta
+}
 
 export interface PopularBlogsResponse {
   data: PopularBlog[]
@@ -8,6 +14,23 @@ export interface PopularQuestionsResponse {
   data: PopularQuestion[]
 }
 
+export type QuestionListData = Omit<
+  QuestionData,
+  'personalize' | 'best_answer_id'
+>
+
+export interface QuestionListMeta {
+  pagination: PaginationResponse
+  sort: QuestionListSortType
+}
+
+export type QuestionListSortType = 'latest'
+
 export interface PopularBlog extends RelatedBlog {}
 
 export interface PopularQuestion extends RelatedQuestion {}
+
+export interface FetchQuestionListPayload {
+  page: number
+  sort: QuestionListSortType
+}
