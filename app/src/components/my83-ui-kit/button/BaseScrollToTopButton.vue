699417796ce84f38e0a7b3988330c7045006cd79
Charlie Lee <charlie800228@gmail.com>
2020-06-10 14:20:21 +0800

    refactor: change ScrollToTopButton to functional component

--- app/src/components/my83-ui-kit/button/BaseScrollToTopButton.vue
+++ app/src/components/my83-ui-kit/button/BaseScrollToTopButton.vue
@@ -1,53 +1,39 @@
-<template>
-  <img
-    class="ScrollToTopButton"
-    src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI0MCIgaGVpZ2h0PSI0MCIgdmlld0JveD0iMCAwIDQwIDQwIj4KICAgIDxwYXRoIGZpbGwtcnVsZT0iZXZlbm9kZCIgZD0iTTIwIDBjMTEuMDQ2IDAgMjAgOC45NTQgMjAgMjBzLTguOTU0IDIwLTIwIDIwUzAgMzEuMDQ2IDAgMjAgOC45NTQgMCAyMCAwem0xLjU3MyA5Ljg3MWMtLjU5My0uNTItMS41MDMtLjQ5NC0yLjA2NS4wODUtNC40MjcgNC41NjMtNy4xNjUgNy40MjQtOC4yNyA4LjY0M2wtLjM4MS40M2MtLjUzNi42MzEtLjQ2IDEuNTc4LjE3MiAyLjExNC42MzEuNTM2IDEuNTc4LjQ2IDIuMTE0LS4xNzJsLjE1OS0uMTgxYy40MjItLjQ3NiAxLjI4Ni0xLjM5NyAyLjU4Mi0yLjc1NWwzLjEwOC0zLjIyOC4wMTkgMTUuMTk1LjAwNy4xNDRjLjA3NC43Ni43MTUgMS4zNTUgMS40OTUgMS4zNTQuODI4LS4wMDEgMS40OTktLjY3MyAxLjQ5OC0xLjUwMmwtLjAxOS0xNS4yOTcgNS45MTIgNi4zMjMuMTEuMTA3Yy41NzMuNSAxLjQ0My40OTUgMi4wMS0uMDM1LjYwNi0uNTY2LjYzNy0xLjUxNS4wNzItMi4xMmwtOC40MTUtOXoiIG9wYWNpdHk9IjEiLz4KPC9zdmc+Cg=="
-    alt="Scroll to top"
-    :class="{ disabled }"
-  />
-</template>
-
 <script lang="ts">
 import Vue from 'vue'
-import { ThisTypedComponentOptionsWithRecordProps } from 'vue/types/options'
+import { FunctionalComponentOptions } from 'vue/types/options'
 import { CombinedVueInstance } from 'vue/types/vue'
 
-export default {
-  props: {
-    disabled: {
-      type: Boolean,
-      default: false,
-    },
+const options: ComponentOption = {
+  functional: true,
+  render(h, ctx) {
+    return h('img', {
+      staticClass: `ScrollToTopButton ${ctx.data.staticClass}`,
+      class: {
+        ...ctx.data.class,
+        disabled: ctx.props.disabled || false,
+      },
+      attrs: {
+        ...ctx.data.attrs,
+        src: `${ctx.props.imageBucketUrl}/front/common/icon/to-top.svg`,
+        alt: 'Scroll to top',
+      },
+      on: ctx.listeners,
+    })
   },
-} as ComponentOption
+}
 
-export type ComponentOption = ThisTypedComponentOptionsWithRecordProps<
-  Instance,
-  Data,
-  Methods,
-  Computed,
-  Props
->
+export type ComponentOption = FunctionalComponentOptions<Props>
 
-export type ComponentInstance = CombinedVueInstance<
-  Instance,
-  Data,
-  Methods,
-  Computed,
-  Props
->
+export type ComponentInstance = CombinedVueInstance<Instance, {}, {}, {}, Props>
 
 export interface Instance extends Vue {}
 
-export interface Data {}
-
-export interface Methods {}
-
-export interface Computed {}
-
 export interface Props {
   disabled: boolean
+  imageBucketUrl: string
 }
+
+export default options
 </script>
 
 <style lang="scss" scoped>
