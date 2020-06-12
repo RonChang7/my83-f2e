5c63d194b5b48a54e6480a211fefcff21acfd77d
Charlie Lee <charlie800228@gmail.com>
2020-06-12 15:16:18 +0800

    fix: remove Promise.allSettled polyfill

---
+++ app/src/utils/promise-all-settled.ts
@@ -0,0 +1,29 @@
+/**
+ * 由於目前透過 map 進行原本 Promise wrapper 無法正確的推斷型別
+ * 導致後續使用無法快速的使用 TypeScript 型別提示的優勢
+ *
+ * 故之後使用方式如下：
+ * Promise.all([promiseWrapper(promise1), promiseWrapper(promise2)])
+ *
+ * @param p A Promise.
+ */
+export const promiseAllSettledWrapper = <T>(
+  p: Promise<T>
+): Promise<PromiseFulfilledResult<T> | PromiseRejectedResult> =>
+  p.then(
+    (val): PromiseFulfilledResult<T> => ({
+      status: 'fulfilled',
+      value: val,
+    }),
+    (err): PromiseRejectedResult => ({ status: 'rejected', reason: err })
+  )
+
+interface PromiseFulfilledResult<T> {
+  status: 'fulfilled'
+  value: T
+}
+
+interface PromiseRejectedResult {
+  status: 'rejected'
+  reason: any
+}
