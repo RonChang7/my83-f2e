09ad4577f483ad61c50d48d1daeb240a61198aee
Charlie Lee <charlie800228@gmail.com>
2020-06-17 17:19:59 +0800

    feat: add missing redirecting rules
    https://app.asana.com/0/0/1180746010069646/f

--- app/src/middleware/redirect/redirect-map.ts
+++ app/src/middleware/redirect/redirect-map.ts
@@ -1,23 +1,41 @@
 import { Route } from 'vue-router'
 
 export const rules: Rule[] = [
+  /**
+   * 討論區 redirect rule
+   *
+   * 舊有路由有以下可能的 URL
+   * 列表：/question, /question/index, /question/index?page=
+   * 搜尋：/question?search=, /question/index?search=, /question/index?search=&page=
+   */
   {
     from: (route) => {
       const { search } = route.query
-      return !!(route.path === '/question' && search)
+      return !!(
+        (route.path === '/question' && search) ||
+        route.path === '/question/index'
+      )
     },
     to: (route) => {
-      const queryMap = route.query
-      const q = queryMap.search
-      delete queryMap.search
+      if (route.query.search) {
+        const queryMap = route.query
+        const q = queryMap.search
+        delete queryMap.search
+
+        return {
+          status: 301,
+          path: '/question/search',
+          query: {
+            ...queryMap,
+            q,
+          },
+        }
+      }
 
       return {
         status: 301,
-        path: '/question/search',
-        query: {
-          ...queryMap,
-          q,
-        },
+        path: '/question',
+        query: route.query,
       }
     },
   },
