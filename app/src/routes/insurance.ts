edfc3552195603abd5583860a180ff0e0cce97eb
Charlie Lee <charlie800228@gmail.com>
2020-09-30 18:00:44 +0800

    feat: adjust route

--- app/src/routes/insurance.ts
+++ app/src/routes/insurance.ts
@@ -1,38 +1,14 @@
-import { NuxtRouteConfig } from '@nuxt/types/config/router'
-import {
-  CreateRouteFunction,
-  NuxtRouteResolve,
-} from '@/nuxt-modules/flexible-routes/module'
-
-const insurancePageList = [
-  { name: 'medical', path: '/medical' },
-  { name: 'casualty', path: '/casualty' },
-  { name: 'criticalIllness', path: '/critical-illness' },
-  { name: 'life', path: '/life' },
-  { name: 'cancer', path: '/cancer' },
-  { name: 'disability', path: '/disability' },
-  { name: 'car', path: '/car' },
-  { name: 'motor', path: '/motor' },
-  { name: 'pet', path: '/pet' },
-  { name: 'travel', path: '/travel' },
-]
-
-const generateInsurancePage = (
-  resolve: NuxtRouteResolve
-): NuxtRouteConfig[] => {
-  return insurancePageList.map((insurance) => {
-    return {
-      name: insurance.name,
-      path: insurance.path,
-      component: resolve('@/views/insurance/page/Index.vue'),
-      meta: {
-        insurance: insurance.path.split('/')[1],
-        requiredStoreModules: ['insurance'],
-      },
-    }
-  })
-}
+import { CreateRouteFunction } from '@/nuxt-modules/flexible-routes/module'
 
 export const createRoutes: CreateRouteFunction = (resolve) => {
-  return [...generateInsurancePage(resolve)]
+  return [
+    {
+      name: 'insuranceList',
+      path: '/insurance/:insurance',
+      component: resolve('@/views/insurance/page/Index.vue'),
+      meta: {
+        requiredStoreModules: ['insurance'],
+      },
+    },
+  ]
 }
