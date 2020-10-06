cb53d38fb562eed0d2124bfd17fdbd27a9913f49
Charlie Lee <charlie800228@gmail.com>
2020-10-06 18:17:20 +0800

    feat: add click action tracking into AEM

--- app/src/analytics/event-listeners/event.type.ts
+++ app/src/analytics/event-listeners/event.type.ts
@@ -4,6 +4,7 @@ export const enum EventTypes {
   PageView = 'PAGE_VIEW',
   SetRole = 'SET_ROLE',
   SetUserId = 'SET_USER_ID',
+  ClickAction = 'CLICK_ACTION',
 }
 
 export type Payloads = {
@@ -13,6 +14,11 @@ export type Payloads = {
   }
   [EventTypes.SetUserId]: {
     userId: number | undefined
+  }
+  [EventTypes.ClickAction]: {
+    category: string
+    action: string
+    label: string
   }
 }
 
