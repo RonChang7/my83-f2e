84151b5c6654078cf8794ed708a9b76c193a1322
Charlie Lee <charlie800228@gmail.com>
2020-10-20 13:15:28 +0800

    fix: layout style

--- app/src/components/insurance/product/ProductCard.vue
+++ app/src/components/insurance/product/ProductCard.vue
@@ -148,12 +148,13 @@ export default options
   }
 
   &__section {
-    flex: 1 0 auto;
+    flex: 1 1 auto;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
 
     &:last-child {
+      flex: 0 0 auto;
       text-align: right;
 
       @include max-media('xl') {
