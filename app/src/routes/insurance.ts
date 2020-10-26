8aae7cd534d5957c9f9f8c16cd018a5374e2f7b3
Charlie Lee <charlie800228@gmail.com>
2020-10-26 12:33:40 +0800

    feat: add insurance product route

--- app/src/routes/insurance.ts
+++ app/src/routes/insurance.ts
@@ -3,6 +3,14 @@ import { CreateRouteFunction } from '@/nuxt-modules/flexible-routes/module'
 export const createRoutes: CreateRouteFunction = (resolve) => {
   return [
     {
+      name: 'insuranceProduct',
+      path: '/insurance/:id(\\d+)',
+      component: resolve('@/views/insurance/product/Index.vue'),
+      meta: {
+        requiredStoreModules: ['insuranceProduct'],
+      },
+    },
+    {
       name: 'insuranceList',
       path: '/insurance/:insurance',
       component: resolve('@/views/insurance/page/Index.vue'),
