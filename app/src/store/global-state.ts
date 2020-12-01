92113451e0b84079cb0e4a6b17a000f848105f8a
Charlie Lee <charlie800228@gmail.com>
2020-12-01 18:17:31 +0800

    refactor: user service

--- app/src/store/global-state.ts
+++ app/src/store/global-state.ts
@@ -2,10 +2,12 @@ import { State as GlobalState } from './global/index'
 import { State as HeaderState } from './header/index'
 import { State as PageMetaState } from './seo/page-meta'
 import { State as JsonLdState } from './seo/json-ld'
+import { State as UserState } from './user/index'
 
 export interface GlobalVuexState {
   global: GlobalState
   header: HeaderState
   pageMeta: PageMetaState
   jsonLd: JsonLdState
+  user: UserState
 }
