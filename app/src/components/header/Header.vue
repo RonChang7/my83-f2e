8320e64da20cc3c7d44c0f9b114381a584215276
Charlie Lee <charlie800228@gmail.com>
2020-12-18 17:29:35 +0800

    feat: add header headline section

--- app/src/components/header/Header.vue
+++ app/src/components/header/Header.vue
@@ -4,15 +4,24 @@
     <div class="ie-info">注意：MY83 保險網不支援 Internet Explorer，為了確保良好的服務體驗，建議您使用其他瀏覽器，例如 Google Chrome、Firefox、Safari、Microsoft Edge。</div>
     <header>
       <template v-if="isDesktop">
-        <DesktopHeader :enable-rwd="true" :user-role="userRole" />
+        <DesktopHeader
+          :header-headline="headerHeadline"
+          :enable-rwd="true"
+          :user-role="userRole"
+        />
       </template>
-      <MobileHeader :enable-rwd="isDesktop" />
+      <MobileHeader
+        :header-headline="headerHeadline"
+        :enable-rwd="isDesktop"
+        :user-role="userRole"
+      />
     </header>
   </Affix>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { differenceInDays } from 'date-fns'
 import { Store } from 'vuex'
 import { ThisTypedComponentOptionsWithRecordProps } from 'vue/types/options'
 import { CombinedVueInstance } from 'vue/types/vue'
@@ -23,10 +32,10 @@ import * as types from '@/store/header/header.type'
 import { GlobalVuexState } from '@/store/global-state'
 import { UserRole } from '@/services/user/user'
 import DeviceMixin, {
-  Computed as DeviceMixinComputed,
+  ComponentInstance as DeviceMixinComponentInstance,
 } from '@/mixins/device/device-mixins'
 import UserMetaMixin, {
-  Computed as UserMetaMixinComputed,
+  ComponentInstance as UserMetaMixinComponentInstance,
 } from '@/mixins/user/user-meta'
 
 export default {
@@ -35,6 +44,19 @@ export default {
     DesktopHeader,
     MobileHeader,
     Affix,
+  },
+  computed: {
+    headerHeadline() {
+      const dueDay = new Date('2021-01-01')
+      const now = new Date()
+      const beforeDueDays = differenceInDays(dueDay, now)
+
+      if (beforeDueDays > 0) {
+        return `失能險停售倒數 ${differenceInDays(dueDay, now)} 天`
+      }
+
+      return ''
+    },
   },
   watch: {
     userRole(val: UserRole) {
@@ -61,7 +83,10 @@ export type ComponentInstance = CombinedVueInstance<
   Props
 >
 
-export interface Instance extends Vue {
+export interface Instance
+  extends Vue,
+    Omit<DeviceMixinComponentInstance, keyof Vue>,
+    Omit<UserMetaMixinComponentInstance, keyof Vue> {
   $store: Store<GlobalVuexState>
 }
 
@@ -69,7 +94,9 @@ export interface Data {}
 
 export type Methods = {}
 
-export interface Computed extends DeviceMixinComputed, UserMetaMixinComputed {}
+export interface Computed {
+  headerHeadline: string
+}
 
 export interface Props {}
 </script>
