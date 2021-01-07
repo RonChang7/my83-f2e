9cf930f1e54fb05f953e69b68a1c045213b39955
Charlie Lee <charlie800228@gmail.com>
2021-01-07 15:03:39 +0800

    feat: add leader board API & Vuex store

--- app/src/store/module-map.ts
+++ app/src/store/module-map.ts
@@ -3,6 +3,7 @@ const moduleMap = {
   questionList: () => import('@/store/question/list'),
   insurance: () => import('@/store/insurance/insurance'),
   insuranceProduct: () => import('@/store/insurance/product'),
+  insuranceLeaderBoard: () => import('@/store/insurance/leader-board'),
 }
 
 export default moduleMap
