8c3cd6dc26f74dfe9d4a3515b5c3f0a5706723e9
Charlie Lee <charlie800228@gmail.com>
2021-01-11 16:27:14 +0800

    feat: make useRoute return a reactive object

--- app/src/utils/composition-api/index.ts
+++ app/src/utils/composition-api/index.ts
@@ -1,5 +1,10 @@
 import { Store } from 'vuex'
-import { getCurrentInstance } from '@nuxtjs/composition-api'
+import {
+  getCurrentInstance,
+  toRefs,
+  reactive,
+  watch,
+} from '@nuxtjs/composition-api'
 
 // @TODO: mock useStore for Vue3 upgrade
 // https://next.vuex.vuejs.org/guide/composition-api.html
@@ -18,8 +23,22 @@ export const useRouter = () => {
   return instance?.$router
 }
 
+// @TODO: mock useRouter, useRoute for Vue3 upgrade
+// Make route as a reactive object
+// Ref: https://school.geekwall.in/p/oV93xZjUQ/reactive-vue-routes-with-the-composition-api
 export const useRoute = () => {
   const instance = getCurrentInstance()
 
-  return instance?.$route
+  const state = reactive({
+    route: instance!.$route,
+  })
+
+  watch(
+    () => instance!.$route,
+    (r) => {
+      state.route = r
+    }
+  )
+
+  return toRefs(state).route
 }
