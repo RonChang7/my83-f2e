8d4e1ca4c46493d7b76127063aac37816f615848
Charlie Lee <charlie800228@gmail.com>
2021-01-12 16:14:35 +0800

    feat: add  leader board page

---
+++ app/src/views/insurance/leader-board/Index.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+import { defineComponent, h } from '@nuxtjs/composition-api'
+import { GlobalVuexState } from '@/store/global-state'
+import { State } from '@/store/insurance/leader-board'
+import { FETCH_LEADER_BOARD } from '@/store/insurance/leader-board.type'
+import { Content } from '@/services/page/Content'
+import { ErrorPageType } from '@/config/error-page.config'
+const InsuranceLeaderBoard = () => import('./InsuranceLeaderBoard.vue')
+
+export default defineComponent({
+  watchQuery: ['sort'],
+  scrollToTop: false,
+  async asyncData(ctx) {
+    const { store, error, query } = ctx
+    const fetchPageData: Promise<any>[] = []
+    fetchPageData.push(
+      store.dispatch(`insuranceLeaderBoard/${FETCH_LEADER_BOARD}`, query.sort)
+    )
+
+    try {
+      await Promise.all([...Content.requests(ctx), ...fetchPageData])
+    } catch (err) {
+      const statusCode = err.response.status === 404 ? err.response.status : 500
+      const message =
+        err.response.status === 404
+          ? ErrorPageType.DEFAULT
+          : ErrorPageType.SERVER
+      return error({
+        statusCode,
+        message,
+      })
+    }
+  },
+  setup() {
+    return () => h(InsuranceLeaderBoard)
+  },
+})
+
+export interface InsuranceLeaderBoardVuexState extends GlobalVuexState {
+  insuranceLeaderBoard: State
+}
+</script>
---
+++ app/src/views/insurance/leader-board/InsuranceLeaderBoard.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="InsuranceLeaderBoard">
+    <LeaderBoardHeaderSection ref="header" />
+
+    <div class="InsuranceLeaderBoard__row">
+      <LeaderBoardListSection />
+    </div>
+
+    <LeaderBoardPromotionSection v-if="shouldShowPromotionSection" />
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  computed,
+  defineComponent,
+  onBeforeUnmount,
+  onMounted,
+  ref,
+} from '@nuxtjs/composition-api'
+import LeaderBoardHeaderSection from '@/components/insurance/section/LeaderBoardHeaderSection.vue'
+import LeaderBoardListSection from '@/components/insurance/section/LeaderBoardListSection.vue'
+import LeaderBoardPromotionSection from '@/components/insurance/section/LeaderBoardPromotionSection.vue'
+import { useDevice } from '@/mixins/device/device-mixins'
+
+export default defineComponent({
+  components: {
+    LeaderBoardHeaderSection,
+    LeaderBoardListSection,
+    LeaderBoardPromotionSection,
+  },
+  setup(_, ctx) {
+    const { isMobile } = useDevice()
+    const header = computed(() => ctx.refs.header as Vue)
+
+    const shouldShowPromotionSection = ref(!isMobile.value)
+    let promotionSectionObserver: IntersectionObserver | null
+
+    onMounted(() => {
+      promotionSectionObserver = isMobile.value
+        ? new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+              shouldShowPromotionSection.value =
+                entry.boundingClientRect.bottom < 0
+            })
+          })
+        : null
+
+      promotionSectionObserver &&
+        promotionSectionObserver.observe(header.value.$el)
+    })
+
+    onBeforeUnmount(() => {
+      promotionSectionObserver && promotionSectionObserver.disconnect()
+      promotionSectionObserver = null
+    })
+
+    return {
+      shouldShowPromotionSection,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+@import '@/sass/variables.scss';
+@import '@/sass/mixins.scss';
+@import '@/sass/rwd.scss';
+
+.InsuranceLeaderBoard {
+  display: flex;
+  flex-direction: column;
+  padding-top: 40px;
+  background: url('#{$image-bucket-url}/front/insurance/leader-board/bg-cloud-desktop.png')
+    center top no-repeat;
+  background-color: $primary-bg;
+
+  @include max-media('sm') {
+    padding-top: 20px;
+    background: url('#{$image-bucket-url}/front/insurance/leader-board/bg-cloud-mobile.png')
+      center top no-repeat;
+    background-color: $primary-bg;
+  }
+
+  @include max-media('xl') {
+    padding-bottom: 60px;
+  }
+
+  &__row {
+    margin: 64px auto 0;
+    width: 1140px;
+
+    @include max-media('xl') {
+      margin: 20px auto 0;
+      width: 100%;
+    }
+  }
+}
+</style>
