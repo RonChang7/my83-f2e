0d83a0a8be49afa7d9d4f0e1804bfb255e97d6c5
Charlie Lee <charlie800228@gmail.com>
2021-01-12 16:14:35 +0800

    feat: add  leader board page

---
+++ app/src/components/insurance/product/LeaderBoardCard.vue
@@ -0,0 +1,270 @@
+<template>
+  <div class="LeaderBoardCard">
+    <div class="LeaderBoardCard__header">
+      <div class="LeaderBoardCard__header__content">
+        <h2 class="LeaderBoardCard__header__title">{{ section.title }}</h2>
+        <div class="LeaderBoardCard__header__description">
+          {{ section.description }}
+        </div>
+      </div>
+      <div class="LeaderBoardCard__header__icon">
+        <BaseLazyImage :image-url="section.image_url" :image-alt="section.id" />
+      </div>
+    </div>
+    <div class="LeaderBoardCard__content">
+      <div
+        v-for="(product, index) in section.products"
+        :key="index"
+        class="LeaderBoardCard__product"
+        @click="() => $router.push({ path: `${product.btn.link.path}` })"
+      >
+        <div class="LeaderBoardCard__product__index">{{ index + 1 }}</div>
+        <div class="LeaderBoardCard__product__content">
+          <div class="company">{{ product.company }}</div>
+          <div class="name">{{ product.name }}</div>
+          <div v-if="product.features.length" class="features">
+            {{ featuresFormatter(product.features) }}
+          </div>
+        </div>
+        <div class="LeaderBoardCard__product__action">
+          <div v-if="product.pageviews" class="count">
+            {{ pageviewsFormatter(product.pageviews) }}
+          </div>
+          <BaseButton :to="product.btn.link" size="m">
+            {{ product.btn.text }}
+          </BaseButton>
+        </div>
+      </div>
+      <GlobalLink
+        :to="`/insurance/${section.id}`"
+        class="LeaderBoardCard__link"
+      >
+        更多商品
+      </GlobalLink>
+    </div>
+    <GlobalLink
+      v-if="section.blog"
+      :to="section.blog.link"
+      class="LeaderBoardCard__blog"
+    >
+      → {{ section.blog.text }}
+    </GlobalLink>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import { Section } from '@/api/insurance/leader-board.type'
+import BaseLazyImage from '@/components/base/lazy-load-image/BaseLazyImage.vue'
+import GlobalLink from '@/components/base/global-link/GlobalLink.vue'
+import BaseButton from '@/components/my83-ui-kit/button/BaseButton.vue'
+import { delimitIntegerWithSymbol } from '@/utils/digital'
+
+export default defineComponent({
+  components: {
+    BaseLazyImage,
+    GlobalLink,
+    BaseButton,
+  },
+  props: {
+    section: {
+      type: Object as () => Section,
+      required: true,
+    },
+  },
+  setup() {
+    const pageviewsFormatter = (number: number) => {
+      return number ? delimitIntegerWithSymbol(number) : ''
+    }
+
+    const featuresFormatter = (features: string[]) => features.join('．')
+
+    return {
+      pageviewsFormatter,
+      featuresFormatter,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+@import '@/sass/variables.scss';
+@import '@/sass/mixins.scss';
+@import '@/sass/rwd.scss';
+
+.LeaderBoardCard {
+  width: 550px;
+  margin-bottom: 64px;
+
+  @include max-media('xl') {
+    width: 100%;
+    margin-bottom: 40px;
+  }
+
+  &__header {
+    @include card-primary;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 24px 30px;
+
+    @include max-media('xl') {
+      @include card-secondary;
+
+      padding: 16px 20px;
+    }
+
+    &__title {
+      margin: 0;
+      color: $gray-primary;
+      font-size: 1.375rem;
+      font-weight: 500;
+
+      @include max-media('xl') {
+        font-size: 1.125rem;
+      }
+    }
+
+    &__description {
+      margin-top: 10px;
+
+      @include max-media('xl') {
+        margin-top: 4px;
+        font-size: 0.875rem;
+      }
+    }
+
+    &__icon {
+      flex: 0 0 auto;
+      width: 94px;
+      height: 94px;
+      margin-left: 10px;
+
+      @include max-media('xl') {
+        width: 60px;
+        height: 60px;
+        margin-left: 8px;
+      }
+    }
+  }
+
+  &__content {
+    @include card-primary;
+
+    margin: 10px 0 14px;
+    padding: 0 36px;
+
+    @include max-media('xl') {
+      @include card-secondary;
+
+      margin: 4px 0 12px;
+      padding: 0 20px;
+    }
+  }
+
+  &__product {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 20px 0;
+    border-bottom: 1px solid $gray-quaternary;
+    cursor: pointer;
+
+    @include max-media('xl') {
+      padding: 12px 0;
+    }
+
+    &__index {
+      flex: 0 0 auto;
+      color: $primary-color;
+      font-size: 2rem;
+      font-weight: 500;
+
+      @include max-media('xl') {
+        font-size: 1.25rem;
+      }
+    }
+
+    &__content {
+      flex: 1 1 auto;
+      padding: 0 20px;
+
+      .company {
+        color: $secondary-color;
+        font-weight: 500;
+      }
+
+      .name {
+        margin-top: 2px;
+        color: $gray-primary;
+        font-size: 1.125rem;
+        font-weight: 500;
+      }
+
+      .features {
+        margin-top: 2px;
+        font-size: 0.875rem;
+      }
+
+      @include max-media('xl') {
+        padding: 0 12px;
+
+        .company,
+        .features {
+          font-size: 0.875rem;
+        }
+
+        .name {
+          font-size: 1rem;
+        }
+      }
+    }
+
+    &__action {
+      flex: 0 0 auto;
+
+      .count {
+        margin-bottom: 6px;
+        color: $gray-primary;
+        font-size: 1.375rem;
+        font-weight: 500;
+        text-align: right;
+
+        &:after {
+          content: '人次';
+          font-size: 0.875rem;
+          font-weight: 400;
+
+          @include max-media('xl') {
+            font-size: 0.75rem;
+          }
+        }
+      }
+    }
+  }
+
+  &__link {
+    display: flex;
+    justify-content: center;
+    padding: 14px 0;
+    font-weight: 500;
+
+    @include max-media('xl') {
+      padding: 10px 0;
+    }
+  }
+
+  &__blog {
+    @include hover('_secondary');
+
+    display: block;
+    padding-left: 16px;
+    font-weight: 500;
+
+    @include max-media('xl') {
+      padding-left: 20px;
+    }
+  }
+}
+</style>
---
+++ app/src/components/insurance/section/LeaderBoardHeaderSection.vue
@@ -0,0 +1,210 @@
+<template>
+  <div class="LeaderBoardHeaderSection">
+    <h1 class="LeaderBoardHeaderSection__title">
+      <img
+        class="LeaderBoardHeaderSection__icon__left"
+        :src="`${$imageBucketUrl}/front/common/icon/willow.svg`"
+        alt="willow-icon"
+      />
+      {{ title }}
+      <img
+        class="LeaderBoardHeaderSection__icon__right"
+        :src="`${$imageBucketUrl}/front/common/icon/willow.svg`"
+        alt="willow-icon"
+      />
+    </h1>
+    <div class="LeaderBoardHeaderSection__description">
+      {{ description }}
+    </div>
+    <div class="LeaderBoardHeaderSection__switch">
+      <div v-if="isMobile" class="LeaderBoardHeaderSection__switch__mobileIcon">
+        <img
+          :src="`${$imageBucketUrl}/front/insurance/leader-board/img-header-crowd-l.png`"
+          alt="crowd-icon"
+        />
+        <img
+          :src="`${$imageBucketUrl}/front/insurance/leader-board/img-header-crowd-r.png`"
+          alt="crowd-icon"
+        />
+      </div>
+      <img
+        v-if="!isMobile"
+        :src="`${$imageBucketUrl}/front/insurance/leader-board/img-header-crowd-l.png`"
+        alt="crowd-icon"
+      />
+      <Affix
+        v-if="isMobile"
+        placeholder-class="fix__switch__wrapper"
+        content-class="fix__switch__content"
+        :affix-offset-top="64"
+        :enable-hide-on-scroll-down="true"
+      >
+        <BaseSwitch :options="options" :value.sync="value" />
+      </Affix>
+      <BaseSwitch v-else :options="options" :value.sync="value" />
+      <img
+        v-if="!isMobile"
+        :src="`${$imageBucketUrl}/front/insurance/leader-board/img-header-crowd-r.png`"
+        alt="crowd-icon"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, ref, watch } from '@nuxtjs/composition-api'
+import { useStore, useRouter, useRoute } from '@/utils/composition-api'
+import { getFirstQuery } from '@/utils/query-string'
+import { InsuranceLeaderBoardVuexState } from '@/views/insurance/leader-board/Index.vue'
+import { Option } from '@/components/my83-ui-kit/input/type'
+import Affix from '@/components/base/affix/Affix.vue'
+import BaseSwitch from '@/components/my83-ui-kit/input/BaseSwitch.vue'
+import DeviceMixin from '@/mixins/device/device-mixins'
+
+export default defineComponent({
+  components: {
+    Affix,
+    BaseSwitch,
+  },
+  mixins: [DeviceMixin],
+  setup() {
+    const store = useStore<InsuranceLeaderBoardVuexState>()
+    const router = useRouter()
+    const route = useRoute()
+
+    const title = computed(() => store.state.insuranceLeaderBoard.title)
+    const description = computed(
+      () => store.state.insuranceLeaderBoard.description
+    )
+
+    const sort = computed(() => getFirstQuery(route?.value.query.sort))
+    const { options, value } = useSwitch({
+      value: sort.value,
+    })
+
+    watch(value, (val) =>
+      router?.push({
+        query: {
+          sort: val.toString() || undefined,
+        },
+      })
+    )
+
+    watch(sort, (val) => {
+      value.value = val
+    })
+
+    return {
+      title,
+      description,
+      options,
+      value,
+    }
+  },
+})
+
+const useSwitch = ({ value }: SwitchPayload) => {
+  const options: Option[] = [
+    {
+      text: '最近熱門',
+      value: '',
+    },
+    {
+      text: 'MY83 專家推薦',
+      value: 'recommend',
+    },
+  ]
+
+  return {
+    options,
+    value: ref(value),
+  }
+}
+
+interface SwitchPayload {
+  value: string | number
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/sass/variables.scss';
+@import '@/sass/rwd.scss';
+
+.LeaderBoardHeaderSection {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: $gray-primary;
+
+  &__icon {
+    &__left,
+    &__right {
+      @include max-media('xl') {
+        width: 29px;
+      }
+    }
+
+    &__right {
+      transform: scaleX(-1);
+    }
+  }
+
+  &__title {
+    margin: 0;
+    font-size: 2.25rem;
+    font-weight: 500;
+
+    @include max-media('xl') {
+      font-size: 1.375rem;
+    }
+  }
+
+  &__description {
+    width: 800px;
+    margin: 16px 0;
+    font-size: 1.125rem;
+
+    @include max-media('xl') {
+      width: 100%;
+      padding: 0 20px;
+      font-size: 0.875rem;
+    }
+  }
+
+  &__switch {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 1440px;
+    width: 100%;
+
+    img {
+      height: 95px;
+    }
+
+    @include max-media('xl') {
+      max-width: auto;
+      flex-direction: column;
+
+      img {
+        position: relative;
+        height: 38px;
+      }
+    }
+  }
+
+  ::v-deep .fix__switch {
+    &__wrapper {
+      width: 100vw;
+    }
+
+    &__content {
+      transition: 0.3s;
+      padding: 8px 20px;
+      margin: -8px 0;
+      background: $primary-bg;
+      z-index: 1;
+    }
+  }
+}
+</style>
