816dafef11b7d173c7de04c76b44c82e38e04040
Charlie Lee <charlie800228@gmail.com>
2021-05-26 15:49:34 +0800

    feat: add useNuxtLinkChecker

--- app/src/utils/composition-api/index.ts
+++ app/src/utils/composition-api/index.ts
@@ -1,3 +1,16 @@
-import { wrapProperty } from '@nuxtjs/composition-api'
+import _ from 'lodash'
+import { useRouter, wrapProperty } from '@nuxtjs/composition-api'
 
 export const useAnalytics = wrapProperty('$analytics', false)
+
+export const useNuxtLinkChecker = () => {
+  const router = useRouter()
+  const isNuxtLink = (path: string) => {
+    const matches = router.getMatchedComponents(path)
+    return !_.isEmpty(matches)
+  }
+
+  return {
+    isNuxtLink,
+  }
+}
