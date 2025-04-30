3c40f9bc09c62dc5814026d1496f1b5871e01747
Ron <zz0188inforce@gmail.com>
2025-04-30 17:55:05 +0800

    feat(insurance): 更新產品標題區域，移除不必要的元素，調整 API 請求邏輯以獲取單一產品詳細資訊，並優化標籤顯示

--- app/src/components/insurance/section/ProductHeaderSection.vue
+++ app/src/components/insurance/section/ProductHeaderSection.vue
@@ -3,76 +3,18 @@
     <div class="ProductHeaderSection__column">
       <div class="ProductHeaderSection__row">
         <div class="ProductHeaderSection__column">
-          <div class="ProductHeaderSection__breadcrumbs">
-            <span v-for="(breadcrumb, index) in breadcrumbs" :key="index">
-              <GlobalLink :to="breadcrumb.link.path">
-                {{ breadcrumb.name }}
-              </GlobalLink>
-            </span>
-          </div>
           <div class="ProductHeaderSection__company">{{ company }}</div>
-        </div>
-        <div
-          v-if="onlineProduct && isMobile"
-          class="ProductHeaderSection__column"
-        >
-          <div class="ProductHeaderSection__onlineProduct">
-            <BaseButton
-              class="active"
-              :to="onlineProduct.link.path"
-              :size="isMobile ? 'm' : 'l-a'"
-              type="secondary"
-              :is-full-width="true"
-            >
-              {{ onlineProduct.text }}
-            </BaseButton>
-          </div>
         </div>
       </div>
       <h1 class="ProductHeaderSection__name">{{ name }}</h1>
-      <div
-        v-if="onlineProduct && !isMobile"
-        class="ProductHeaderSection__onlineProduct"
-      >
-        <BaseButton
-          class="active"
-          :to="onlineProduct.link.path"
-          :size="isMobile ? 'm' : 'l-a'"
-          type="secondary"
-          :is-full-width="true"
-        >
-          {{ onlineProduct.text }}
-        </BaseButton>
-      </div>
-      <div
-        v-if="badges.length || rankingBadge"
-        class="ProductHeaderSection__featureTags"
-      >
-        <ProductFeatureTag
-          v-for="(tag, index) in badges"
-          :key="index"
-          :tag="tag"
-        />
-        <div v-if="rankingBadge" class="ProductHeaderSection__rankingTag">
-          <BaseTag small type="primary-outline">
-            {{ rankingBadge.text }}
-          </BaseTag>
-          <span>{{ rankingBadge.description }}</span>
-        </div>
-      </div>
-      <div class="ProductHeaderSection__features">{{ features }}</div>
-      <!-- <div v-if="viewCount" class="ProductHeaderSection__viewCount">
-        有
-        <span>{{ viewCount }}</span>
-        人有興趣
-      </div> -->
+      <div class="ProductHeaderSection__tags">{{ tags }}</div>
     </div>
     <div class="ProductHeaderSection__column">
       <div class="ProductHeaderSection__files">
         <BaseButton
           v-for="(file, index) in files"
           :key="index"
-          :to="file.link.url"
+          :to="file.link"
           :size="isMobile ? 's' : 'm'"
           type="tertiary"
           target="_blank"
@@ -88,58 +30,55 @@
 import { computed, defineComponent, useStore } from '@nuxtjs/composition-api'
 import { InsuranceProductVuexState } from '@/views/insurance/product/Index.vue'
 import { useDevice } from '@/mixins/device/device-mixins'
-import GlobalLink from '@/components/base/global-link/GlobalLink.vue'
 import BaseButton from '@/components/my83-ui-kit/button/BaseButton.vue'
-import BaseTag from '@/components/my83-ui-kit/tag/BaseTag.vue'
-import ProductFeatureTag from '../product/ProductFeatureTag.vue'
 
 export default defineComponent({
   components: {
-    GlobalLink,
     BaseButton,
-    BaseTag,
-    ProductFeatureTag,
   },
   setup() {
     const store = useStore<InsuranceProductVuexState>()
     const { isMobile } = useDevice()
-    const breadcrumbs = computed(
-      () => store.state.pageMeta.pageMeta?.breadcrumbs
-    )
+
     const company = computed(
-      () => store.state.insuranceProduct.product?.product.company
+      () => store.state.insuranceProduct.singleProduct?.basic.companyName
     )
     const name = computed(
-      () => store.state.insuranceProduct.product?.product.name
+      () => store.state.insuranceProduct.singleProduct?.basic.productName
     )
-    const features = computed(() =>
-      store.state.insuranceProduct.product?.product.features.join('．')
-    )
-    const viewCount = computed(
-      () => store.state.insuranceProduct.product?.product.view_count
-    )
-    const files = computed(() => store.state.insuranceProduct.product?.files)
-    const onlineProduct = computed(
-      () => store.state.insuranceProduct.product?.online_product
-    )
-    const badges = computed(
-      () => store.state.insuranceProduct.product?.product.badges
-    )
-    const rankingBadge = computed(
-      () => store.state.insuranceProduct.product?.product.ranking_badge
-    )
+    const tags = computed(() => {
+      const tags = []
+      store.state.insuranceProduct.singleProduct?.basic.productTag.forEach(
+        (item) => {
+          tags.push(item.content)
+        }
+      )
+      return tags.join('．')
+    })
+
+    const files = computed(() => {
+      const mapping = {
+        productTerm: '商品條款',
+        productTable: '商品費率',
+        productDM: '商品DM',
+        productRule: '投保規則',
+      }
+      const basic = store.state.insuranceProduct.singleProduct?.basic || {}
+
+      return Object.entries(mapping)
+        .filter(([key]) => basic[key])
+        .map(([key, text]) => ({
+          text,
+          link: basic[key],
+        }))
+    })
 
     return {
       isMobile,
-      breadcrumbs,
       company,
       name,
-      features,
-      viewCount,
+      tags,
       files,
-      onlineProduct,
-      badges,
-      rankingBadge,
     }
   },
 })
@@ -164,18 +103,6 @@ export default defineComponent({
     justify-content: space-between;
   }
 
-  &__breadcrumbs {
-    font-size: 0.875rem;
-
-    span:after {
-      content: '>';
-    }
-
-    a {
-      @include hover('_gray-secondary-lighter');
-    }
-  }
-
   &__company {
     color: $secondary-color;
     font-size: 1.375rem;
@@ -189,40 +116,6 @@ export default defineComponent({
     font-size: 1.75rem;
     font-weight: 500;
     margin-bottom: 6px;
-  }
-
-  &__onlineProduct {
-    width: 136px;
-    padding: 6px 0 8px;
-  }
-
-  &__featureTags {
-    @include inline-block-space-remove-parent;
-
-    & > * {
-      @include inline-block-space-remove-child;
-      display: inline-block;
-      margin: 0 8px 6px 0;
-    }
-  }
-
-  &__rankingTag {
-    > span {
-      margin-left: 3px;
-      color: $primary-color;
-      font-size: 0.875em;
-      font-weight: 500;
-    }
-  }
-
-  &__features,
-  &__viewCount {
-    font-size: 0.875rem;
-  }
-
-  &__viewCount > span {
-    font-size: 1.25rem;
-    font-weight: 500;
   }
 
   &__files {
@@ -246,21 +139,6 @@ export default defineComponent({
     &__name {
       font-size: 1.375rem;
       margin-bottom: 8px;
-    }
-
-    &__onlineProduct {
-      width: 124px;
-      padding: 0;
-      float: right;
-    }
-
-    &__features,
-    &__viewCount {
-      font-size: 0.75rem;
-    }
-
-    &__viewCount > span {
-      font-size: 1rem;
     }
 
     &__files {
